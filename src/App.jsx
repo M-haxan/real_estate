5ccfb41e4cd5a46d92645587c547b54a3e597709
Muhammad Hassan <hassanbalouch716@gmail.com>
2026-06-15 17:33:23 +0500

    create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/sign-up' element={<Signup/>}/>
+          <Route path='/sign-in' element={<Signin/>}/>
+        </Routes>
+      </BrowserRouter>
+    
   )
 }
 
