9f3eb05d9b2ac1cdecdbf3de8d28fa326994cf70
Muhammad Hassan <hassanbalouch716@gmail.com>
2026-06-16 16:07:59 +0500

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 const app = express();
-
+app.use(express.json());
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
@@ -13,3 +14,4 @@ app.listen(port, () => {
   console.log(`API listening at http://localhost:${port}`);
 });
 app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
