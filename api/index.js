501bdf5e2f4415c95f08283bb3dc3fc4fc39ee47
Muhammad Hassan <hassanbalouch716@gmail.com>
2026-06-16 15:32:33 +0500

    Create User model

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+const app = express();
+
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
-const app = express();
 const port = 3000;
 app.listen(port, () => {
   console.log(`API listening at http://localhost:${port}`);
 });
+app.use('/api/users', userRouter);
