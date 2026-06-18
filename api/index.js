0a53113b1318766d88a751be57dfc8ed888f6759
Muhammad Hassan <hassanbalouch716@gmail.com>
2026-06-18 23:14:28 +0500

    cretae userupdate api

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 app.use(express.json());
 dotenv.config();
+app.use(cookieParser());
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
@@ -13,7 +15,7 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`API listening at http://localhost:${port}`);
 });
-app.use('/api/users', userRouter);
+app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
 app.use((err, req, res, next) => {
